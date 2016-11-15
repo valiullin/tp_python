a13ce1c630600ec2ce152cff3b8b97d3439941ed
Adel Valiullin <valiullin@users.noreply.github.com>
2016-11-15 23:46:26 +0300

    6th problem solved

---
+++ HW1/euler6.py
@@ -0,0 +1,8 @@
+# Sum square difference
+# Problem 6
+# Find the difference between the sum of the squares of the first one hundred 
+# natural numbers and the square of the sum.
+
+
+n = 100
+print(int((3*n**2 + 2*n) * (n**2 - 1) / 12))
